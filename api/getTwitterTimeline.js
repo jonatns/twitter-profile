d25789bd47f8629c8339b19932abf1a0e072bfb4
Jonathan Navarrete <jnavarrete@prosper.com>
2018-12-28 15:43:10 -0800

    Fix get twiter timeline

--- api/getTwitterTimeline.js
+++ api/getTwitterTimeline.js
@@ -1,13 +1,3 @@
-const Twitter = require("twitter-node-client").Twitter;
-
-const twitterConfig = {
-  consumerKey: process.env.TWITTER_CONSUMER_KEY,
-  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
-
-const twitter = new Twitter(twitterConfig);
 require("now-env");
 const Twitter = require("twitter-node-client").Twitter;
 
